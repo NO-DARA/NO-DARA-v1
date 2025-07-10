21ca60532f43f08d758c36fb2ace209102fe591e
NO-DARA <tombot422@gmail.com>
2025-07-10 16:20:30 +0100

    config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=cVBihTjR#uWBXAL4T1c5RsjwEzluQpX4jE9d3SRBvFqOEUXWJ-ow",
 MONGODB: process.env.MONGODB || "",//enter mongo db url
 };
